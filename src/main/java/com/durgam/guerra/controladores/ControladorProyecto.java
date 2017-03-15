1294042537be9996deeb79510d6bad14819eeb53
Javier Guerra <jguerra@Desktop-jguerra>
2017-03-15 15:30:50 -0300

    EntidadProyecto

---
+++ src/main/java/com/durgam/guerra/controladores/ControladorProyecto.java
@@ -0,0 +1,10 @@
+package com.durgam.guerra.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class ControladorProyecto {
+
+}
---
+++ src/main/java/com/durgam/guerra/modelos/Proyecto.java
@@ -0,0 +1,38 @@
+package com.durgam.guerra.modelos;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Proyecto {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String nombreProyecto;
+	private String descripcionProyecto;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombreProyecto() {
+		return nombreProyecto;
+	}
+	public void setNombreProyecto(String nombreProyecto) {
+		this.nombreProyecto = nombreProyecto;
+	}
+	public String getDescripcionProyecto() {
+		return descripcionProyecto;
+	}
+	public void setDescripcionProyecto(String descripcionProyecto) {
+		this.descripcionProyecto = descripcionProyecto;
+	}
+
+	
+	
+}
