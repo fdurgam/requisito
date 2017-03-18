5b897b449bc93d2ddf1d01ebfd4fde83db813e6e
Javier <fernandojavierguerra@gmail.com>
2017-03-18 11:35:53 -0300

    Cambios

--- src/main/java/com/durgam/guerra/modelos/Requisito.java
+++ src/main/java/com/durgam/guerra/modelos/Requisito.java
@@ -7,11 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.Data;
 
 @Entity
-@Data
-
 public class Requisito {
 	
 	@Id
@@ -23,5 +20,49 @@ public class Requisito {
 	private String CreadoPor;
 	private String Tipo;
 	private String Prioridad;
+
+	
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getResumen() {
+		return Resumen;
+	}
+	public void setResumen(String resumen) {
+		Resumen = resumen;
+	}
+	public Date getFechaCreacion() {
+		return FechaCreacion;
+	}
+	public void setFechaCreacion(Date fechaCreacion) {
+		FechaCreacion = fechaCreacion;
+	}
+	public String getDescripcion() {
+		return Descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+	public String getCreadoPor() {
+		return CreadoPor;
+	}
+	public void setCreadoPor(String creadoPor) {
+		CreadoPor = creadoPor;
+	}
+	public String getTipo() {
+		return Tipo;
+	}
+	public void setTipo(String tipo) {
+		Tipo = tipo;
+	}
+	public String getPrioridad() {
+		return Prioridad;
+	}
+	public void setPrioridad(String prioridad) {
+		Prioridad = prioridad;
+	}
 
 }
